00d1ef191b3346b8d9acbe003f9fcf24ebf36329
Brandon Berney <brandon.berney@louisville.edu>
2020-10-12 16:44:35 -0400

    upv: Add exit functionality

--- Prog2/Prog2/Prog2Form.cs
+++ Prog2/Prog2/Prog2Form.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Grading ID: E3780
+ * Program: 2
+ * Due Date: October 19 2020
+ * Course: CIS 200-76
+ * Description: Provides backend code for the Program 2 form.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,15 +20,35 @@ namespace UPVApp
 {
     public partial class Prog2Form : Form
     {
+        /*
+         * Preconditions: None
+         * Postcondition: Form is initialized.
+         */
+
         public Prog2Form()
         {
             InitializeComponent();
         }
 
+        /*
+         * Preconditions: The about button is clicked.
+         * Postcondition: The about window appears.
+         */
+
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
             AboutBox about = new AboutBox();
             about.ShowDialog();
+        }
+
+        /*
+         * Preconditions: The exit button is clicked.
+         * Postcondition: The program exits.
+         */
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
